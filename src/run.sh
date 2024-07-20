cb086946f5c710913b9357a7d0af4ff20ae60db4
SkyTheCodeMaster <34724753+SkyTheCodeMaster@users.noreply.github.com>
2024-07-20 00:21:35 -0400

    patch ld library path

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,9 @@
 #/bin/bash
 
 source venv/bin/activate
+
+# Patch LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=`python3 -c 'import os; import nvidia.cublas.lib; import nvidia.cudnn.lib; print(os.path.dirname(nvidia.cublas.lib.__file__) + ":" + os.path.dirname(nvidia.cudnn.lib.__file__))'`
+
+
 python3.11 main.py
